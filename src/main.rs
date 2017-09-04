67bd906c938cdb660a06279c000f7c32ada32b57
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2017-09-04 00:06:20 -0400

    Cmdline parsing and matching integrated
    Now we just need to build in the actual functionality.

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,76 @@
 extern crate log;
 extern crate env_logger;
 extern crate chrono;
+extern crate clap;
 
 mod mop_structs;
-
-use chrono::prelude::*;
+mod mop_act;
 
 use std::env;
+
 use log::{LogRecord, LogLevelFilter};
 use env_logger::LogBuilder;
+use chrono::prelude::*;
+use clap::{Arg, App, SubCommand};
+
 use mop_structs::Metadata as Metadata;
 
-fn init_logging(){
+fn init_logging(log_level: &str){
     let format = |record: &LogRecord| {
         let dt = Local::now();
-        format!("{} {} [{}]: {}", dt.format("%Y-%m-%d %H%M%S").to_string(), record.location().file(), record.level(), record.args())
+        format!("{} [{}]: {}", dt.format("%Y-%m-%d %H%M%S").to_string(), record.level(), record.args())
     };
 
     let mut builder = LogBuilder::new();
     builder.format(format).filter(None, LogLevelFilter::Info);
-
-    //If building in debug, always check
+    
+    //If building in debug, force debug
     if cfg!(debug_assertions) {
         builder.parse("debug");
-    }  else if env::var("MOP_LOG").is_ok() {
-        builder.parse(&env::var("MOP_LOG").unwrap());
+    } else {
+        builder.parse(log_level);
     }
-
     builder.init().unwrap();
 }
 
 fn main(){
-    init_logging();
-    info!("starting up");
+    //Parse command line args and check the sub command
+    let args = App::new("MOP - Metadata Organization Program")
+                        .version(env!("CARGO_PKG_VERSION"))
+                        .author(env!("CARGO_PKG_AUTHORS"))
+                        .about("Cleans up messy music files!")
+                        .subcommand(SubCommand::with_name("all")
+                                    .about("Perform all operations available (excluding 'help' subcommand)"))                       
+                        .subcommand(SubCommand::with_name("check")
+                                    .about("Verify the given directory and print info about it"))
+                        .subcommand(SubCommand::with_name("clean")
+                                    .about("Do a full fix of all file metadata"))
+                        .subcommand(SubCommand::with_name("cover-art")
+                                    .about("Retrieve the cover art for all file, if possible"))
+                        .subcommand(SubCommand::with_name("rename")
+                                    .about("Rename the file in some specific manner"))
+                        .arg(Arg::with_name("directory")
+                            .short("i")
+                            .long("working-dir")
+                            .value_name("PATH")
+                            .help("Set the working directory of the program")
+                            .required(true)
+                            .takes_value(true))
+                        .arg(Arg::with_name("log")
+                            .short("v")
+                            .long("log")
+                            .value_name("debug|info|warn|error")
+                            .help("Set the logging verbosity of the program (Default: 'warn')")
+                            .takes_value(true))
+                        .get_matches();
 
-    let testy: Metadata = mop_structs::new_metadata();
-    debug!("testy.x={}", testy.is_complete);
+    init_logging(args.value_of("log").unwrap_or("warn"));
+    info!("Starting up MOP");
 
-    // Test to see that we can build
-    let line_args = env::args().skip(1);
-    
-    for argument in line_args {
-        println!("{}", argument);
+    //TODO: Complete this match!
+    match args.subcommand_name() {
+        Some("check") => mop_act::quick_check(String::from("")),
+        None        => panic!("No subcommand was used - Not supported yet!"),
+        _           => panic!("The subcommand that was used that is not supported yet"),
     }
 }
---
+++ src/mop_act.rs
@@ -0,0 +1,9 @@
+//Actions of the program as simple, public functions
+
+use std::fs;
+use std::string::String;
+
+pub fn quick_check(curr_dir: String){
+    info!("It works")
+    
+}
