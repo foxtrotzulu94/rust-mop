149165484459508c0d77215448db5e034adc44a6
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2017-09-11 22:53:38 -0400

    find_artist_id now uses XML Wrapper
    It looks MUCH better.

--- src/src_music_brainz.rs
+++ src/src_music_brainz.rs
@@ -1,52 +1,45 @@
 //All the functions needed to use the MusicBrainz API (https://musicbrainz.org/doc/Development/XML_Web_Service/Version_2)
 use mop_structs::{SongFile,BasicMetadata};
 use mop_online::{make_get_request, percent_encode};
+use xml_wrap::{XmlMap};
 
-use std::io::{Error, ErrorKind, self};
 use std::str;
-use curl::easy::Easy;
+use std::io::{Error, ErrorKind, self};
 use xml::reader::{EventReader, XmlEvent};
 
 static API_ENDPOINT: &'static str = "https://musicbrainz.org/ws/2/";
 
 //TODO: Better error handling (i.e. away from std::io::Result)
 
-fn find_artist_id(xml_parser: EventReader<&[u8]>) -> io::Result<String>{
+fn find_artist_id(raw_data: &str) -> io::Result<String>{
     let mut artist_id = String::new();
     let mut confidence = 75;
-    for e in xml_parser {
-        match e {
-            Ok(XmlEvent::StartElement { name, attributes, .. }) => {
-                if name.local_name.contains("artist") {
-                    let mut some_artist_id = String::new();
-                    let mut curr_confidence=0;
-                    //Find score attribute and artist ID
-                    for attr in attributes{
-                        match attr.name.local_name.as_str(){
-                            "id" => some_artist_id=attr.value,
-                            "score" => curr_confidence = attr.value.parse::<i32>().unwrap(),
-                            _ => (),
-                        }
-                    }
-                    if curr_confidence > confidence{
-                        artist_id = some_artist_id;
-                        debug!("{} confidence:{}",artist_id,confidence);
-                        break;
-                    }
-                }
+
+    let data = XmlMap::from_str(raw_data);
+    let possible_artists = &data.root["metadata"]["artist-list"].get_matching_children("artist");
+
+    for an_artist in *possible_artists{
+        let mut some_artist_id = String::new();
+        let mut curr_confidence=0;
+        for (name, value) in an_artist.attributes.iter(){
+            match name.as_str() {
+                "id" => some_artist_id = value.clone(),
+                "score" => curr_confidence = value.parse::<i32>().unwrap(),
+                _ => (),
             }
-            Err(e) => {
-                error!("Error: {:?}", e);
-                return Err(Error::new(ErrorKind::Other, e.msg()))
-            }
-            _ => {}
+        }
+        if curr_confidence > confidence{
+            artist_id = some_artist_id;
+            confidence = curr_confidence;
+            debug!("{} confidence:{}",artist_id,confidence);
+            break;
         }
     }
 
     Ok(artist_id)
 }
 
-fn find_recording_data(xml_parser: EventReader<&[u8]>) -> io::Result<BasicMetadata>{
+fn find_recording_data(raw_data: &str) -> io::Result<BasicMetadata>{
     //TODO: get the request and return a well formed metadata struct instance
 
     //Two steps: 
@@ -54,43 +47,10 @@ fn find_recording_data(xml_parser: EventReader<&[u8]>) -> io::Result<BasicMetada
     //Go through recordings
     let mut temp_metadata = BasicMetadata::new();
     let mut recording_list : Vec<BasicMetadata> = Vec::new();
-    let mut is_reading_release = false;
-    for e in xml_parser {
-        match e {
-            Ok(XmlEvent::StartElement { name, attributes, .. }) => {
-                //TODO: populate the list
-                match name.local_name.as_str() {
-                    "release" => is_reading_release = true,
-                    "title" => {
-                        if temp_metadata.album.is_empty(){
-                            //TODO: apparently we have to get the next "event", XmlEvent::Characters
-                            // ref: https://github.com/Enet4/dicom-rs/blob/b13c13facf5ddcedce92344f86c334109f958aa6/dictionary_builder/main.rs#L226
-                        }
-                    }
-                    _ => {}
-                }   
-            }
-            Ok(XmlEvent::EndElement { name }) => {
-                match name.local_name.as_str() {
-                    "recording"|"release" => {
-                        is_reading_release = false;
-                        //Save and continue with next recording
-                        if temp_metadata.has_some_data(){
-                            recording_list.push(temp_metadata);
-                        }
-                        temp_metadata = BasicMetadata::new();
-                    }
-                    
-                    _ => {}
-                }  
-            }
-            Err(e) => {
-                error!("Error: {:?}", e);
-                return Err(Error::new(ErrorKind::Other, e.msg()))
-            }
-            _ => {}
-        }
-    }
+    
+    let data = XmlMap::from_str(raw_data);
+    let possible_recordings = &data.root["metadata"]["recording-list"].get_matching_children("recording");
+
 
     //Select best one (i.e. go for first release ever)
 
@@ -109,8 +69,7 @@ pub fn check(song_file: &SongFile) -> io::Result<()>{
 
     //MusicBrainz always replies in XML
     let curly_result = make_get_request(API_ENDPOINT, &artist_request).unwrap();
-    let parser = EventReader::from_str(curly_result.as_str());
-    let artist_id = find_artist_id(parser)?;
+    let artist_id = find_artist_id(curly_result.as_str())?;
     //TODO: Check if the artist was found or error
     warn!("{} - Artist ID={}",song_file.metadata.artist().unwrap(), artist_id);
 
@@ -123,7 +82,7 @@ pub fn check(song_file: &SongFile) -> io::Result<()>{
     debug!("Request-url:{}",song_request.as_str());
     let curly_result_two = make_get_request(&API_ENDPOINT, &song_request).unwrap();
     //TODO: Check if more than one song was returned or error!
-    warn!("{}",curly_result_two.as_str());
+    // warn!("{}",curly_result_two.as_str());
 
     //TODO:
     //Get all album info (Sort by year, if possible)
--- src/xml_wrap.rs
+++ src/xml_wrap.rs
@@ -11,6 +11,7 @@ pub struct XmlNode{
     //FIXME: Right now, as is written, using the Parent var will cause Heap Corruption
     // parent: Option<Box<XmlNode>>,
     pub value: String,
+    pub attributes: HashMap<String,String>,
     children: HashMap<String,Vec<Box<XmlNode>>>,
 }
 
@@ -19,6 +20,7 @@ impl XmlNode{
         return XmlNode{
             // parent: None,
             value: String::new(),
+            attributes: HashMap::new(),
             children: HashMap::new(),
         }
     }
@@ -27,22 +29,42 @@ impl XmlNode{
         return Box::new(XmlNode::new());
     }
 
-    fn add_new_child(&mut self, key: String, val: String) -> &XmlNode{
+    pub fn add_new_child(&mut self, key: String, val: String) -> &XmlNode{
         // unsafe{
-        let mut new_node = Box::new(XmlNode { /*parent: Some(Box::from_raw(self)),*/ value: val, children: HashMap::new()});
+        let mut new_node = Box::new(
+            XmlNode { 
+                /*parent: Some(Box::from_raw(self)),*/ 
+                value: val, 
+                attributes: HashMap::new(),
+                children: HashMap::new()
+            });
         let mut list = self.children.entry(key).or_insert(Vec::new());
         list.push(new_node);
         return list.last().unwrap();
         // }
     }
 
-    fn add_child(&mut self, key: String, mut node: Box<XmlNode>) -> &XmlNode{
+    pub fn add_attribute(&mut self, key: String, val: String){
+        self.attributes.insert(key,val);
+    }
+
+    pub fn add_child(&mut self, key: String, mut node: Box<XmlNode>) -> &XmlNode{
         // unsafe{
             // node.parent = Some(Box::from_raw(self));
         let mut list = self.children.entry(key).or_insert(Vec::new());
         list.push(node);
         return list.last().unwrap();
         // }
+    }
+
+    pub fn get_child(&mut self, key: String) -> &XmlNode{
+        let list = self.children.get(&String::from(key)).unwrap();
+        return list.first().unwrap();
+    }
+
+    pub fn get_matching_children(&self, key: &str) -> &Vec<Box<XmlNode>>{
+        let list = self.children.get(&String::from(key)).unwrap();
+        return list;
     }
 }
 
@@ -89,6 +111,9 @@ impl XmlMap{
                 Ok(XmlEvent::StartElement { name, attributes, .. }) => {
                     debug!("Adding {}", name.local_name);
                     let mut child = XmlMap::create(xml_parser);
+                    for attr in attributes{
+                        child.add_attribute(attr.name.local_name, attr.value);
+                    }
                     ret_val.add_child(name.local_name, child);
                 }
                 Ok(XmlEvent::Characters(data)) => {
@@ -116,8 +141,8 @@ impl XmlMap{
 
     pub fn from_str(raw_data : &str) -> XmlMap{
         let mut streaming_parser = EventReader::from_str(raw_data);
-        let mut root = XmlMap::create(&mut streaming_parser);
-        let mut dstruct = XmlMap{ root: root, raw_data: String::from(raw_data)};
+        let root = XmlMap::create(&mut streaming_parser);
+        let dstruct = XmlMap{ root: root, raw_data: String::from(raw_data)};
         return dstruct;
     }
 }
